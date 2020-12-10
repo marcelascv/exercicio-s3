9d136f31c70934af40b0a499a575a3a20455c824
marcelascv <63434138+marcelascv@users.noreply.github.com>
2020-12-09 22:40:57 -0300

    Respostas exercicios-semana.js

--- exercicios-semana.js
+++ exercicios-semana.js
@@ -18,6 +18,16 @@ for (let contador = 0; contador < 10; contador++){
 
 console.log(resultado)
 
+//RESPOSTA
+
+function potenciaDeDois(num) {
+  let resultado = 1
+  while (num > 0) {
+    resultado *= 2
+    num -= 1
+  }
+  return resultado
+}
 
 // 2) Crie uma função que receba três números e determine se um número é maior que outro.
 // Exemplo:
@@ -35,6 +45,26 @@ function numeroMaior(num1, num2, num3) {
 console.log(numeroMaior(1, 2, 3))
 console.log(numeroMaior(40, 16, 20))
 
+//RESULTADO
+
+function retornaNumMaior(num1, num2, num3) {
+  
+  let maior = num1;
+
+  if (num1 > num2) {
+    if (num1 > num3) {
+      maior = num1
+    } else {
+      maior = num3
+    }
+  } else if (num2 > num3) {
+    maior = num2
+  } else {
+    maior = num3
+  }
+  return maior
+}
+
 // ***************
 
 // DESAFIOS!
@@ -51,6 +81,19 @@ function numeroPrimo(numero) {
 console.log(numeroPrimo(10))
 console.log(numeroPrimo(3))
 
+//RESPOSTA
+function numPrimo(num) {
+  if (num === 1) {
+    return true
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+
 // Crie uma função que conte o número de vogais em uma `string` e retorne o número de vogais. 
 // Exemplo:
 // function contaVogais("carro") // 2 
@@ -58,6 +101,31 @@ console.log(numeroPrimo(3))
 const numero = 'string'
 console.log(numero.length)
 
+//RESPOSTA
+
+function contaVogais(string) {
+  const vogais = 'aeiou'
+  const arrayMinusculas = string.toLowerCase()
+  let totalVogais = 0
+
+  for (let i = 0; i < arrayMinusculas.length; i += 1) {
+    for (let j = 0; j < vogais.length; j += 1) {
+      if (arrayMinusculas[i] === vogais[j]) {
+        totalVogais += 1
+      }
+    }
+  }
+
 // Use estruturas de controle para inverter uma array de inteiros. A função deverá receber uma array de números e retornar uma array com a ordem dos elementos invertida. Não é válido utilizar `arr.reverse()`.
 // Exemplo:
 // function inverteArray([1, 2, 3, 4]) // [4, 3, 2, 1]
+
+//RESPOSTA
+    
+function inverteArray(array) {
+  const arrayInvertida = []
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    arrayInvertida.push(array[i])
+  }
+  return arrayInvertida
+}
